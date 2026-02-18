f800100c06a7715bbe0bb4ce4cc2bcb83f5f8177
TS-2210 <148909343+TS-2210@users.noreply.github.com>
2026-02-18 11:55:22 +0000

    guessing works, now need user input

--- Hangman.java
+++ Hangman.java
@@ -1,18 +1,80 @@
 public class Hangman {
     private String playerName;
     private String wordToGuess;
+    final public int MAX_GUESSES = 20;
+    private char[] guesses = new char[MAX_GUESSES];
     public Hangman(String playerName) {
         this.playerName = playerName;
     }
+    
+    public String getCurrentState() {
+        String currentState = "";
+        boolean found = false;
+        for(char c : this.getWord().toCharArray()) {
+            for(char g : this.getGuesses()) {
+                if (c==g) {
+                    currentState += c;
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) {
+                currentState += "_";
+            }
+            found = false;
+        }     
+        return currentState;
+    }
+
+    public char[] getGuesses() {
+        return guesses;
+    }
+
+    public int getRemainingGuesses() {
+        int count = 0;
+        for(char c : guesses) {
+            if (c != '\0') {
+                count++;
+            }
+        }
+        return MAX_GUESSES - count;
+    }
+
+    public boolean tryChar(char guess) {
+        boolean contains = false;
+        for(char c : this.getGuesses()) {
+            if(c == guess) {
+                contains = true;
+                break;
+            }
+        }
+        if(contains) {
+            return false;
+        }
+        else {
+            if(this.getWord().indexOf(guess) != -1) {
+                this.getGuesses()[MAX_GUESSES - this.getRemainingGuesses()] = guess;
+                return true;
+            }
+            else {
+                this.getGuesses()[MAX_GUESSES - this.getRemainingGuesses()] = guess;
+                return false;
+            }
+        }
+    }
+
     public String getPlayerName() {
         return playerName;
     }
+
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
     }
+
     public String getWord() {
         return wordToGuess;
     }
+
     public void setWord(String wordToGuess) {
         this.wordToGuess = wordToGuess;
     }
--- HangmanDriver.java
+++ HangmanDriver.java
@@ -2,9 +2,20 @@ import java.io.Console;
 public class HangmanDriver {
     public static void main(String args[]){
         Console console = System.console();
-        Hangman hangman = new Hangman();
+        HangmanDictionary dictionary = new HangmanDictionary();
         String name = console.readLine("Enter your name: ");
-        hangman.setPlayerName(name);
+        Hangman hangman = new Hangman(name);
+        String word = dictionary.getWord();
+        hangman.setWord(word);
+        hangman.tryChar('a');
+        hangman.tryChar('e');
+        hangman.tryChar('i');
+        hangman.tryChar('o');
+        hangman.tryChar('u');
         console.printf("Welcome to Hangman, %s!", hangman.getPlayerName());
+        System.out.println();
+        console.printf("Word to guess: %s", hangman.getWord());
+        System.out.println();
+        console.printf("Current state: %s", hangman.getCurrentState());
     }
 }
